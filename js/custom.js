947c0cc2e15860f40925dde1100628e3f140c650
dennyjohnk <denny.john@asista.com>
2021-01-20 13:47:39 +0530

    menu bar update

--- js/custom.js
+++ js/custom.js
@@ -429,12 +429,3 @@ $(document).on('click','a.frame-close', function(e){
   $('.header-preview').slideUp(); 
 });
 
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5efaefc99e5f694422918fd2/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
